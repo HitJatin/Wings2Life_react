1a6372f7530b3bd1c21efed967ecf12461f0a44d
Jatin <adriano23012001@gmail.com>
2020-08-25 18:21:32 +0530

    Current Code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React,{ useState,Component } from 'react';
+import React,{Component } from 'react';
 import './App.css';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Navbar,NavbarBrand } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter,Navbar,NavbarBrand,Table} from 'reactstrap';
 import $ from 'jquery';
-var level=1,selected=0;
+var level=1,i;
 var configuration = {
 	branding: {"name" : "SkillPill" },
 	title: "Values",
 	category: "Baselines",
 	icon: "./skillpill.png",
 	url: "https://ix61k6qun9.execute-api.ap-southeast-1.amazonaws.com/prod/lifetoolsdataset",
-	levelDetails: [{count:2 ,rule: "minimum" }, 
-				   {count:1, rule: "exact"},
-                   {count:1, rule: "exact"}]
+	levelDetails: [{count:8 ,rule: "minimum" }, 
+                  {count:7, rule: "exact"},
+                  {count:6, rule: "exact"},
+                  {count:5, rule: "exact"},
+                  {count:4, rule: "exact"},
+                  {count:3, rule: "exact"},
+                  {count:2, rule: "exact"},
+                  {count:1, rule: "exact"}]
 }
+
 $("#favlogo").attr('href',configuration.icon);
 function DataButton(props) {
   return (
@@ -25,47 +31,83 @@ class DataSet extends Component {
     let idButton="qual_"+i;
     return (
       <DataButton
+          key={idButton}
           idButton={idButton}
-          value={item[i].Meaning}
+          value={item[i].Property}
           onClick={() => this.props.onClick(i)}
         />
     )
   }
   render() {
-    const rows = [];
-    let i;
-    let item=this.props.item;
-
-    for (i=0;i<item.length;i++)
-    {
-      if(item[i].Level==level){
-        rows.push(
-        this.renderButton(i)
-        );
+    const rows=[];    
+    for (i=0;i<this.props.item.length;i++)
+      {
+        if(level<=configuration.levelDetails.length){
+          if(this.props.item[i].Level===String(level)){
+            rows.push(
+            this.renderButton(i)
+            );
+          }
+        }
       }
-    }
-    return <div className="itemGroup">{rows}</div>
+      return (<div className="itemGroup">{rows}</div>)
   }
 }
 
-
+class OrderSet extends Component {
+  render() {
+    var i,j,k=1;
+    const rows=[];
+    for(i=(configuration.levelDetails.length+1);i>(configuration.levelDetails.length-5);i--){
+    for(j=0;j<this.props.item.length;j++)
+      if(this.props.item[j].Level===String(i)){
+        rows.push(
+          <tr>
+            <th scope="row">{k}</th>
+            <td>{this.props.item[j].Property}</td>
+            <td>{this.props.item[j].Meaning}</td>
+          </tr>
+        )
+        k++;
+      }
+    }
+    if(level===(configuration.levelDetails.length+1))
+    return (
+    <Table striped>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>Belief</th>
+          <th>Counter-Belief</th>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </Table>
+    );
+    else return null;
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modal: true,
-      item:[]    
+      item:[],
+      selected:0    
     };
     this.toggle = this.toggle.bind(this);
+    this.submit = this.submit.bind(this);
+    this.updateStatusLevel = this.updateStatusLevel.bind(this);
+    this.levelCheck = this.levelCheck.bind(this);
   }
 
 
   componentDidMount() {
-    const apiUrl = this.props.api.apiUrl;
+    const apiUrl = this.props.initial.apiUrl;
     fetch(apiUrl)
       .then((response) => response.json())
-      .then((data) => this.setState(this.state.item=data,console.log(data)));
+      .then((data) => this.setState({item:data}));
   }
   toggle() {
     this.setState({
@@ -73,55 +115,85 @@ class App extends Component {
     });
   }
   updateSelection(i) {
-    var property = document.getElementById('qual_'+i);
-    if(configuration.levelDetails[level-1].rule=="exact" && this.state.item[i].Status==0 && configuration.levelDetails[level-1].count==selected)
+    let property = document.getElementById('qual_'+i);
+    if(configuration.levelDetails[level-1].rule==="exact" && this.state.item[i].Status==='0' && configuration.levelDetails[level-1].count===this.state.selected)
     return;
-    if(this.state.item[i].Status==0)
-    {   property.style.backgroundColor="#00cc00"
-        this.state.item[i].Status=1;
-        selected++;
+    else if(this.state.item[i].Status==='0')
+    {
+      property.style.backgroundColor="#00cc00";
+      let testItem = Object.assign({}, this.state);
+      testItem.item[i].Status = "1";
+      this.setState(testItem);
+      this.setState({
+        selected:(this.state.selected+1)
+      })
         //$("#currentCount").text(selected+levelCount());
     }
-    else if(this.state.item[i].Status==1)
+    else if(this.state.item[i].Status==='1')
     {
-        property.style.backgroundColor="#0000FF"
-        this.state.item[i].Status=0;
-        selected--;
+      property.style.backgroundColor="#0000FF";
+      let testItem = Object.assign({}, this.state);
+      testItem.item[i].Status = "0";
+      this.setState(testItem);
+      this.setState({
+        selected:(this.state.selected-1)
+      })
         //$("#currentCount").text(selected+levelCount());
     }
   }
-  levelCheck = () => {
-    if(configuration.levelDetails[level-1].rule=="minimum" && configuration.levelDetails[level-1].count<=selected)
+  levelCheck(){
+    if(configuration.levelDetails[level-1].rule==="minimum" && configuration.levelDetails[level-1].count<=this.state.selected)
     {
          return "true";
     }
-     else if(configuration.levelDetails[level-1].rule=="exact" && configuration.levelDetails[level-1].count==selected)
+     else if(configuration.levelDetails[level-1].rule==="exact" && configuration.levelDetails[level-1].count===this.state.selected)
     {
         return "true";
     }
     else
     return "false";
   }
-  submit = () => {
-    console.log(this.state.item);
-    if (this.levelCheck()=="true")
+  submit(){
+    if (this.levelCheck()==="true")
     {
-      console.log("test"); 
+      this.setState({
+        selected:0
+      })
+      //this.updateStatusLevel();
+    }
+  }
+  updateStatusLevel()
+  {
+    console.log("test");
+    this.setState({
+      selected:0
+    })
+    level++;
+    for(i=0;i<this.state.item.length;i++)
+    {
+      if(this.state.item[i].Status==='1')
+      {
+        let testItem = Object.assign({}, this.state);
+        testItem.item[i].Level=String(parseInt(testItem.item[i].Level)+1);
+        testItem.item[i].Status='0';
+        document.getElementById('qual_'+i).style.backgroundColor="#0000FF";
+        this.setState(testItem);
+      }
     }
   }
 
   
   render() {
-    return <div>
+    return (<div>
     <div className="Header" >
             <Navbar fixed="top" dark color ="dark" >
               <div className="container">
-                <NavbarBrand ><img src={configuration.icon} height="70" width="70" />Select your {configuration.title}</NavbarBrand>
+                <NavbarBrand ><img src={this.props.initial.icon} height="70" width="70" />Select your {this.props.initial.title}</NavbarBrand>
               </div>
             </Navbar>
         </div>
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
-          <ModalHeader toggle={this.toggle}><img src={configuration.icon} height="70" width="70" />{configuration.branding.name}</ModalHeader>
+          <ModalHeader toggle={this.toggle}><img src={this.props.initial.icon} height="70" width="70" />{this.props.initial.name}</ModalHeader>
           <ModalBody>test</ModalBody>
           <ModalFooter>
             <Button color='primary' onClick={this.toggle}>Do Something</Button>{' '}
@@ -129,15 +201,14 @@ class App extends Component {
           </ModalFooter>
         </Modal>
         <DataSet item={this.state.item} onClick={i => this.updateSelection(i)}/>
+        <OrderSet item={this.state.item}/>
         <div className="Footer">
-
-
             <Navbar fixed="bottom" dark color="dark" text="light">
-                <Button onClick={this.submit} variant="dark" size="lg" block>Submit</Button>
+                <Button onClick={this.submit} variant="dark" size="lg" block>Submit {this.state.selected}</Button>
             </Navbar>
         </div>
     </div>
-  }
+    )}
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const config = {
-  apiUrl: "https://ix61k6qun9.execute-api.ap-southeast-1.amazonaws.com/prod/lifetoolsdataset?category=Baselines"
+  branding: {"name" : "SkillPill" },
+	title: "Values",
+	category: "Baselines",
+	icon: "./skillpill.png",
+  apiUrl: "https://ix61k6qun9.execute-api.ap-southeast-1.amazonaws.com/prod/lifetoolsdataset?category=Baselines",
+	levelDetails: [{count:2 ,rule: "minimum" }, 
+				   {count:1, rule: "exact"},
+                   {count:1, rule: "exact"}]
 };
 
 ReactDOM.render(
   <React.StrictMode>
-    <App api={config}/>
+    <App initial={config}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
